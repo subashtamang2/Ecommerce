afcc9cb839947709c94f5fea2ac35aa1ae0aa09b
SubashLama123 <lamasubaah2@gmail.com>
2025-06-20 11:50:31 +0545

    ch

--- frontend/src/components/Common/Navbar.jsx
+++ frontend/src/components/Common/Navbar.jsx
@@ -4,10 +4,15 @@ import { HiOutlineUser, HiOutlineShoppingBag, HiBars3BottomRight, HiMagnifyingGl
 import SearchBar from './SearchBar';
 import CartDrawer from '../Layout/CartDrawer';
 import { IoMdClose } from 'react-icons/io';
+import { useSelector } from 'react-redux';
 const Navbar = () => {
 
   const [drawerOpen, setDrawerOpen] = useState(false);
   const [navDrawerOpen, setNavDrawerOpen] = useState(false);
+  const { cart } = useSelector((state) => state.cart);
+
+  const cartItemCount =
+    cart?.products?.reduce((total, product) => total + product.quantity, 0) || 0;
 
   const toggleNavDrawer = () => {
     setNavDrawerOpen(!navDrawerOpen);
@@ -48,8 +53,10 @@ const Navbar = () => {
 
           <button onClick={toggleCartDrawer}
             className='relative hover:text-black'>
+
             <HiOutlineShoppingBag className='h-6 w-6 text-gray-700' />
-            <span className='absolute -top-1 bg-[#ea2e0e]  text-white text-xs rounded-full px-2 py-0.5'>4</span>
+            {cartItemCount > 0 && (<span className='absolute -top-1 bg-[#ea2e0e]  text-white text-xs rounded-full px-2 py-0.5'>{cartItemCount}</span>)}
+
           </button>
 
           {/* Search */}
